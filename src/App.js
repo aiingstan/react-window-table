8614c6cf6080c89aa538723d0f758d7dce8aef2c
yufeng <yufeng.c.guo@gmail.com>
2018-12-02 14:32:20 +0800

    tried scroll event

--- src/App.js
+++ src/App.js
@@ -2,59 +2,79 @@ import React from 'react';
 import AutoSizer from 'react-virtualized-auto-sizer';
 import SyncTables from './SyncTables';
 
-export default () => (
-  <div
-    style={{
-      display: 'flex',
-      flexDirection: 'column',
-      flexGrow: 1,
-      background: 'white',
-      zIndex: 50,
-    }}
-  >
-    <div style={{ textAlign: 'center' }}>Header</div>
-    <div style={{ display: 'flex', flexGrow: 1 }}>
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      activeRowIndex: null
+    };
+  }
+
+  onLeftGridClick = e => {
+    const { rowIndex } = e.target.dataset;
+    this.setState({
+      activeRowIndex: rowIndex
+    });
+  };
+
+  render() {
+    return (
       <div
         style={{
           display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          flexBasis: 120,
+          flexDirection: 'column',
+          flexGrow: 1,
+          background: 'white',
+          zIndex: 50,
         }}
       >
-        Sidebar
+        <div style={{ textAlign: 'center' }}>Header</div>
+        <div style={{ display: 'flex', flexGrow: 1 }}>
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              flexBasis: 120,
+            }}
+          >
+            Sidebar
+          </div>
+          <div style={{ flex: '1 1 auto' }}>
+            <AutoSizer>
+              {({ width, height }) => (
+                <div
+                  style={{
+                    display: 'relative',
+                    width,
+                    height,
+                    backgroundColor: 'coral',
+                    color: 'rgba(255,255,255,0.9)',
+                  }}
+                  onClick={this.onLeftGridClick}
+                >
+                  <SyncTables
+                    width={width}
+                    height={height}
+                    activeRowIndex={this.state.activeRowIndex}
+                  />
+                </div>
+              )}
+            </AutoSizer>
+          </div>
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              flexBasis: 120,
+            }}
+          >
+            Sidebar
+          </div>
+        </div>
+        <div style={{ textAlign: 'center' }}>Footer</div>
       </div>
-      <div style={{ flex: '1 1 auto' }}>
-        <AutoSizer>
-          {({ width, height }) => (
-            <div
-              style={{
-                display: 'relative',
-                width,
-                height,
-                backgroundColor: 'coral',
-                color: 'rgba(255,255,255,0.9)',
-              }}
-            >
-              <SyncTables
-                width={width}
-                height={height}
-              />
-            </div>
-          )}
-        </AutoSizer>
-      </div>
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          flexBasis: 120,
-        }}
-      >
-        Sidebar
-      </div>
-    </div>
-    <div style={{ textAlign: 'center' }}>Footer</div>
-  </div>
-)
+    )
+  }
+}
--- src/SyncTables.js
+++ src/SyncTables.js
@@ -1,52 +1,53 @@
 import React from 'react';
 import { FixedSizeGrid } from 'react-window';
 import scrollbarSize from 'dom-helpers/util/scrollbarSize';
-import { Dropdown, Menu } from 'antd';
-import 'antd/dist/antd.css';
+// import { Dropdown, Menu } from 'antd';
+// import 'antd/dist/antd.css';
 // import SimpleBar from 'simplebar/dist/simplebar';
 // import 'simplebar/dist/simplebar.css';
 
 const columnCount = 25;
-const rowCount = 1000;
+const rowCount = 200;
 const columnWidth = 100;
 const rowHeight = 42;
 
-const menu = (
-  <Menu>
-    <Menu.Item key="0">
-      <a href="http://www.alipay.com/">1st menu item</a>
-    </Menu.Item>
-    <Menu.Item key="1">
-      <a href="http://www.taobao.com/">2nd menu item</a>
-    </Menu.Item>
-    <Menu.Divider />
-    <Menu.Item key="3">3rd menu item</Menu.Item>
-  </Menu>
-);
+// const menu = (
+//   <Menu>
+//     <Menu.Item key="0">
+//       <a href="http://www.alipay.com/">1st menu item</a>
+//     </Menu.Item>
+//     <Menu.Item key="1">
+//       <a href="http://www.taobao.com/">2nd menu item</a>
+//     </Menu.Item>
+//     <Menu.Divider />
+//     <Menu.Item key="3">3rd menu item</Menu.Item>
+//   </Menu>
+// );
 
 export default class SyncTables extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.headerGrid = React.createRef();
     this.leftGrid = React.createRef();
     this.dataGrid = React.createRef();
     this.dataGridOuter = React.createRef();
-    this.state = {
-      activeRowIndex: null
-    }
+    this.dataGridInner = React.createRef();
+    this.HeaderCell = this.HeaderCell.bind(this);
+    this.BodyCell = this.BodyCell.bind(this);
   }
 
   componentDidMount() {
     // const grid = this.dataGrid.current;
     // new SimpleBar(this.dataGridOuter.current);
-    console.log(this.dataGridOuter.current);
+    // console.log(this.dataGridOuter.current);
     // this.dataGrid.current.style.backgroundColor='red';
+    const dataGridInner = this.dataGridInner.current;
+    console.log(dataGridInner);
+    // dataGridInner.addEventListener('scroll', this.scrollEvent)
   }
 
-  setActiveRowIndex = rowIndex => {
-    this.setState({
-      activeRowIndex: rowIndex
-    });
+  scrollEvent = e => {
+    console.log(e);
   }
 
   HeaderCell = ({columnIndex, style}) => (
@@ -55,114 +56,122 @@ export default class SyncTables extends React.Component {
     </div>
   )
 
-  BodyCell = ({columnIndex, rowIndex, style}) => (
+  LeftCell = ({rowIndex, style}) => (
     <div
-      style={{ ...style, color: (rowIndex === this.state.activeRowIndex ? 'black' : 'white') }}
+      style={style}
+      data-row-index={rowIndex}
+      className={rowIndex === parseInt(this.props.activeRowIndex) ? 'active': 'normal'}
     >
-      {`(${rowIndex}, ${columnIndex})`}
+      <div>{`S[${rowIndex}]`}</div>
+      {/* {!isScrolling ?
+        <div>{`S[${rowIndex}]`}</div> :
+        <div>{'...'}</div>
+      } */}
     </div>
   )
 
+  BodyCell = ({columnIndex, rowIndex, style}) => {
+    return (
+      <div
+        style={style}
+        data-row-index={rowIndex}
+        className={rowIndex === parseInt(this.props.activeRowIndex) ? 'active': 'normal'}
+      >
+        {`(${rowIndex}, ${columnIndex})`}
+      </div>
+    )
+  }
+
   render() {
     const { width, height } = this.props;
-
+    console.log('rendering all')
     return [
-        <div
-          style={{
-            overflow: 'hidden',
-            backgroundColor: 'yellow',
-            position: 'absolute',
-            color: 'black',
-            textAlign: 'center',
-            width: columnWidth,
-            height: rowHeight,
-          }}
-        >
-          {`${width}x${height}`}
-        </div>,
+      <div
+        style={{
+          overflow: 'hidden',
+          backgroundColor: 'yellow',
+          position: 'absolute',
+          color: 'black',
+          textAlign: 'center',
+          width: columnWidth,
+          height: rowHeight,
+        }}
+        key="left-top-grid"
+      >
+        {`${width}x${height}`}
+      </div>,
 
-        // left side column
-        <FixedSizeGrid
-          columnCount={1}
-          rowCount={rowCount}
-          columnWidth={columnWidth}
-          rowHeight={rowHeight}
-          width={columnWidth}
-          height={height - rowHeight - scrollbarSize()}
-          ref={this.leftGrid}
-          style={{
-            overflow: 'hidden',
-            backgroundColor: 'lightgray',
-            position: 'absolute',
-            top: rowHeight
-          }}
-          useIsScrolling={true}
-        >
-          {({rowIndex, style, isScrolling}) => (
-            <div
-              style={{ ...style, color: (rowIndex === this.state.activeRowIndex ? 'black' : 'white') }}
-              onClick={this.setActiveRowIndex.bind(this, rowIndex)}
-            >
-              {!isScrolling ?
-                <Dropdown
-                  overlay={menu}
-                  trigger={['click']}
-                >
-                  <div>{`S[${rowIndex}]`}</div>
-                </Dropdown> :
-                <div>{'...'}</div>
-              }
-            </div>
-          )}
-        </FixedSizeGrid>,
+      // left column
+      <FixedSizeGrid
+        columnCount={1}
+        rowCount={rowCount}
+        columnWidth={columnWidth}
+        rowHeight={rowHeight}
+        width={columnWidth}
+        height={height - rowHeight - scrollbarSize()}
+        ref={this.leftGrid}
+        style={{
+          overflow: 'hidden',
+          backgroundColor: 'lightgray',
+          position: 'absolute',
+          top: rowHeight
+        }}
+        useIsScrolling={false}
+        key="left-grid"
+      >
+        {this.LeftCell}
+      </FixedSizeGrid>,
 
-        // header
-        <FixedSizeGrid
-          columnCount={columnCount}
-          rowCount={1}
-          columnWidth={columnWidth}
-          rowHeight={rowHeight}
-          height={rowHeight}
-          width={width - columnWidth}
-          ref={this.headerGrid}
-          style={{
-            overflow: 'hidden',
-            backgroundColor: 'lightgray',
-            position: 'absolute',
-            left: columnWidth
-          }}
-        >
-          {this.HeaderCell}
-        </FixedSizeGrid>,
+      // header
+      <FixedSizeGrid
+        columnCount={columnCount}
+        rowCount={1}
+        columnWidth={columnWidth}
+        rowHeight={rowHeight}
+        height={rowHeight}
+        width={width - columnWidth}
+        ref={this.headerGrid}
+        style={{
+          overflow: 'hidden',
+          backgroundColor: 'lightgray',
+          position: 'absolute',
+          left: columnWidth
+        }}
+        key="top-grid"
+      >
+        {this.HeaderCell}
+      </FixedSizeGrid>,
 
-        // main data grid
-        <FixedSizeGrid
-          columnCount={columnCount}
-          rowCount={rowCount}
-          columnWidth={columnWidth}
-          rowHeight={rowHeight}
-          height={height - rowHeight}
-          width={width - columnWidth + scrollbarSize()}
-          ref={this.dataGrid}
-          outerRef={this.dataGridOuter}
-          // When a scroll occurs in the body grid,
-          // synchronize the scroll position of the header grid
-          onScroll={({ scrollLeft, scrollTop }) =>
-            {
-              this.headerGrid.current.scrollTo({ scrollLeft })
-              this.leftGrid.current.scrollTo({ scrollTop })
-            }
-
+      // main data grid
+      <FixedSizeGrid
+        columnCount={columnCount}
+        rowCount={rowCount}
+        columnWidth={columnWidth}
+        rowHeight={rowHeight}
+        height={height - rowHeight}
+        width={width - columnWidth + scrollbarSize()}
+        ref={this.dataGrid}
+        outerRef={this.dataGridOuter}
+        innerRef={this.dataGridInner}
+        // // When a scroll occurs in the body grid,
+        // // synchronize the scroll position of the header grid
+        onScroll={({ scrollLeft, scrollTop }) =>
+          {
+            this.headerGrid.current.scrollTo({ scrollLeft })
+            this.leftGrid.current.scrollTo({ scrollTop })
           }
-          style={{
-            position: 'absolute',
-            left: columnWidth,
-            top: rowHeight,
-          }}
-        >
-          {this.BodyCell}
-        </FixedSizeGrid>
-      ]
+
+        }
+        style={{
+          position: 'absolute',
+          left: columnWidth,
+          top: rowHeight,
+        }}
+        key="data-grid"
+      >
+        {this.BodyCell}
+      </FixedSizeGrid>
+    ]
 
   }
 }
